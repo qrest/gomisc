ab4cbec0c17fadc7628f3f6466486f2d18e2270a
qrest <qrest@protonmail.com>
2024-06-25 22:04:51 +0200

    make adapters and middleware public

--- network/network.go
+++ network/network.go
@@ -4,6 +4,7 @@ import "net/http"
 
 type Adapter func(http.Handler) http.Handler
 
+// Adapt constructs a sequence of http.Handler which act as a middleware.
 func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
 	for i := len(adapters) - 1; i >= 0; i-- {
 		h = adapters[i](h)
@@ -11,12 +12,17 @@ func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
 	return h
 }
 
-// maxBodyConfig limits the amount of bytes which can be read from the request body to size (in number of MiB).
-func maxBodyConfig(size int64) Adapter {
+// MaxBody limits the amount of bytes which can be read from the request body to size (in number of MiB).
+func MaxBody(size int64) Adapter {
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			r.Body = http.MaxBytesReader(w, r.Body, 1024*1024*size)
 			h.ServeHTTP(w, r)
 		})
 	}
+}
+
+// MaxBody5MiB limits the amount of bytes which can be read from the request body to 5 MiB.
+func MaxBody5MiB() Adapter {
+	return MaxBody(5)
 }
